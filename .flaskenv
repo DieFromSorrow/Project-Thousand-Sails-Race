4ca55b62154aa4ca3aef2f219755cd10f7f331e9
Last-Night-On-Earth <2937128647@qq.com>
2023-01-25 17:35:25 +0800

    init_project

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=thousand_sails_race
+FLASK_ENV=development
---
+++ thousand_sails_race/views.py
@@ -0,0 +1,7 @@
+
+
+from thousand_sails_race import app
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
