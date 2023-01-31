74953f92e5158ef2d17d63ed36c1559e31e843bf
Last-Night-On-Earth <2937128647@qq.com>
2023-01-31 15:57:53 +0800

    new change

---
+++ thousand_sails_race/blueprints/index.py
@@ -0,0 +1,8 @@
+
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for, session, flash
+
+bp = Blueprint("index", __name__)
+
+@bp.route('/')
+def index():
+    return render_template('index.html')
