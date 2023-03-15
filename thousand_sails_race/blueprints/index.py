cfd0233ce4d4fc1759cd5b175ddfae6cfb79fb48
Last-Night-On-Earth <2937128647@qq.com>
2023-03-15 20:10:57 +0800

    debug

--- thousand_sails_race/blueprints/index.py
+++ thousand_sails_race/blueprints/index.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, render_template, request, jsonify, redirect, url_for, session, flash
+from flask import Blueprint, render_template
 
 from thousand_sails_race.models import HotraceinfoModel, ExperienceModel, RaceinfoModel
 
@@ -8,7 +8,9 @@ bp = Blueprint('index', __name__)
 
 @bp.route('/')
 def index():
-    hotrace = HotraceinfoModel.query.order_by(HotraceinfoModel.start_time.desc()).all()
+    hot_race = HotraceinfoModel.query.order_by(HotraceinfoModel.start_time.desc()).all()
     experience = ExperienceModel.query.limit(3).all()
-    selected_race=RaceinfoModel.query.limit(10).all()
-    return render_template('index.html',hot_race=hotrace,experience=experience,selected_race=selected_race)
+    selected_race = RaceinfoModel.query.limit(10).all()
+    return render_template('index.html', hot_race=hot_race,
+                           experience=experience,
+                           selected_race=selected_race)
